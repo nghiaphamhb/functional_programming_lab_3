202f5e39e43e2530a5d23f1c05475feef6ef03de
second <phamdangtrungnghia@gmail.com>
2025-12-01 01:21:35 +0300

    feat(interp): implement linear interp's module

--- my_lab3/lib/interp.ml
+++ my_lab3/lib/interp.ml
@@ -9,3 +9,18 @@ module type S = sig
   (* [eval pts x] calculate y interpolation at x, based on points list [pts].*)
 end
 
+(* Linear interpolation (2 points) *)
+module Linear : S = struct
+  let name = "linear"
+
+  let eval pts x =
+    match pts with
+    | p1 :: p2 :: _ ->
+        let x1, y1 = (p1.x, p1.y) in
+        let x2, y2 = (p2.x, p2.y) in
+        if x2 = x1 then invalid_arg "Linear.eval: x1 = x2 (division by zero)";
+        let t = (x -. x1) /. (x2 -. x1) in
+        y1 +. (t *. (y2 -. y1))
+    | _ -> invalid_arg "Linear.eval: need at least 2 points"
+end
+
