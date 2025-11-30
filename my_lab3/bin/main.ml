85669a9cc6dfddf3be35bf438d698732462212d5
second <phamdangtrungnghia@gmail.com>
2025-12-01 01:26:51 +0300

    feat: linear interpolation's utils

--- my_lab3/bin/main.ml
+++ my_lab3/bin/main.ml
@@ -58,3 +58,31 @@ let get_config () =
   in
   parse_args default_config args
 
+(** Function parses input line into point's record parse_line : string ->
+    Point.t *)
+let parse_line (line : string) : Point.t option =
+  let line = String.trim line in
+  if line = "" then None
+  else
+    (* separators = ';' or '\t' or ' ' *)
+    let sep =
+      if String.contains line ';' then ';' (* x;y *)
+      else if String.contains line '\t' then '\t' (* x[tab]y *)
+      else ' ' (* x y *)
+    in
+    let parts =
+      line |> String.split_on_char sep |> List.filter (fun s -> s <> "")
+      (* filter empty char *)
+    in
+    match parts with
+    | [ sx; sy ] ->
+        let x = float_of_string sx in
+        let y = float_of_string sy in
+        Some { x; y }
+    | _ ->
+        prerr_endline ("Cannot parse line: " ^ line);
+        None
+
+(** function prints result print_result : string -> float -> float -> unit *)
+let print_result algo_name x y = Printf.printf "> %s: %g %g\n%!" algo_name x y
+
