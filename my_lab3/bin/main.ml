737cee54baec3613e497becb5f0024729a354cc5
second <phamdangtrungnghia@gmail.com>
2025-12-01 01:26:18 +0300

    feat: config's utils
    Function to parse command line arguments into config record

--- my_lab3/bin/main.ml
+++ my_lab3/bin/main.ml
@@ -14,3 +14,47 @@ type config = {
 let default_config =
   { use_linear = false; use_newton = false; step = 0.5; newton_n = 4 }
 
+(** parse_args : config -> string list -> config Function to parse command line
+    arguments into config record. *)
+let rec parse_args cfg args =
+  match args with
+  (* End of recursion *)
+  | [] -> cfg
+  (* linear interpolation *)
+  | "--linear" :: rest ->
+      let cfg' = { cfg with use_linear = true } in
+      parse_args cfg' rest
+  (* newton interpolation *)
+  | "--newton" :: rest ->
+      let cfg' = { cfg with use_newton = true } in
+      parse_args cfg' rest
+  (* step size *)
+  | "--step" :: value :: rest ->
+      let step =
+        try float_of_string value
+        with Failure _ -> failwith ("--step expects a float, got: " ^ value)
+      in
+      let cfg' = { cfg with step } in
+      parse_args cfg' rest
+  (* number of points for newton *)
+  | "-n" :: value :: rest ->
+      let n =
+        try int_of_string value
+        with Failure _ -> failwith ("-n expects an int, got: " ^ value)
+      in
+      let cfg' = { cfg with newton_n = n } in
+      parse_args cfg' rest
+  (* unknown argument *)
+  | arg :: _ -> failwith ("Unknown argument: " ^ arg)
+
+(** Call this function to active function parse_args get_config : unit -> config
+*)
+let get_config () =
+  let args =
+    Sys.argv |> Array.to_list |> function
+    | [] -> []
+    | _prog_name :: rest -> rest
+    (* Skip argv[0] *)
+  in
+  parse_args default_config args
+
